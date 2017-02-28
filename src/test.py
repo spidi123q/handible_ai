d4c48cd4ce2ff5891d061fe6f8b370b24c695b0d
spidi123q <spidi123q@gmail.com>
2017-02-28 21:38:15 +0530

    init files

---
+++ src/test.py
@@ -0,0 +1,19 @@
+import sklearn
+import math
+
+_author_ = 'wombat'
+
+class Solver:
+    def demo(self):
+
+        a = int(input("a "))
+        b = int(input("b "))
+        c = int(input("c "))
+        d = b ** 2 - 4 * a * c
+        disc = math.sqrt(d)
+        root1 = (-b + disc) / (2 * a)
+        root2 = (-b - disc) / (2 * a)
+        print(root1, root2)
+        print(root1, root2)
+
+Solver().demo()
